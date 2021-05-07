5aa59dc23be8fd589ba7df7569e35c1ab113ea74
Devskiller <owl@devskiller.com>
2021-05-07 07:41:23 +0000

    Candidate repo initialization

---
+++ src/main/java/com/devskiller/utils/StringHelper.java
@@ -0,0 +1,12 @@
+package com.devskiller.utils;
+
+public class StringHelper {
+
+    public static String reverse(final String input) {
+        return null;
+    }
+
+    public static String substring(final String input, final int index) {
+        return null;
+    }
+}
