7cb05ea1b1ea43e5b26125a6bdbcf47000670a18
Sturdayz <nakkrawee.k@ku.th>
2021-05-10 16:07:10 +0700

    Update StringHelper.java

--- src/main/java/com/devskiller/utils/StringHelper.java
+++ src/main/java/com/devskiller/utils/StringHelper.java
@@ -3,10 +3,29 @@ package com.devskiller.utils;
 public class StringHelper {
 
     public static String reverse(final String input) {
-        return null;
+        String result = "";
+        if (input.length() > 0) {
+            for (int i = input.length()-1; i>=0 ; i--) {
+                result += input.charAt(i);
+            }
+        }
+        else{
+            throw new IllegalArgumentException("illegalArgumentException");
+        }
+        return result;
     }
 
+
+
     public static String substring(final String input, final int index) {
-        return null;
+        String result = "";
+        String splitSpace[] = input.split(" ");
+        if (splitSpace.length >= index && !input.isEmpty()) {
+            result = splitSpace[index - 1];
+        }
+        else {
+            throw new IllegalArgumentException("illegalArgumentException");
+        }
+        return result;
     }
 }
